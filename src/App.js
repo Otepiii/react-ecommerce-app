ea6a74013ec00a577d4457707bd13aabd3b655aa
joseph Evangelista <oootep123@gmail.com>
2021-03-21 21:26:55 -0700

    added images to products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Products from './components/Products/Products'
+import Products from './components/Products/Products';
 
 const App = () => {
     return (
         <div>
-            <Products/>
+            <Products />
         </div>
     )
 }
